6ae268475e560f957d6969a18c716c8f5dfd6c12
The-3rio <craftmenlab@gmail.com>
2024-10-26 12:33:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "production") {
-	app.use(cors({origin: "http://localhost:5173",credentials: true,}));
+	app.use(cors({origin: "https://hello-world-frontend.onrender.com",credentials: true,}));
 }
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
